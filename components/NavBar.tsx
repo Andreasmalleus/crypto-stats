520292445131a35d4210385f05d327433e179144
Andreasmalleus <andreasmalleus@gmail.com>
2022-01-08 18:09:35 +0200

    header style and font

---
+++ components/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../styles/navbar.module.scss";
+
+export const NavBar: React.FC = () => {
+  return (
+    <div>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <div className={styles.title}>CryptoMarketStats</div>
+          <div className={styles.options}>
+            <div>Cyptocurrencies</div>
+            <div>Exchanges</div>
+            <div>Favorites</div>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <button className={styles.loginButton}>Log in</button>
+          <button className={styles.signupButton}>Sign up</button>
+          <input className={styles.searchButton} placeholder="Search" />
+        </div>
+      </header>
+    </div>
+  );
+};
