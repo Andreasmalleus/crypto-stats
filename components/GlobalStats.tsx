7a642d29322f1aa1a25aabc9b0c93c09a8368164
Andreasmalleus <andreasmalleus@gmail.com>
2022-01-08 18:59:51 +0200

    few style changes

--- components/GlobalStats.tsx
+++ components/GlobalStats.tsx
@@ -16,26 +16,30 @@ export const GlobalStats = () => {
     <div className={styles.globalStats}>
       <div className={styles.globalStatsContainer}>
         <div>
-          <span>Cryptos: </span>
+          <span>Cryptos:</span>
           <span>{total_cryptocurrencies}</span>
         </div>
         <div>
-          <span>Exchanges: </span>
+          <span>Exchanges:</span>
           <span>{active_exchanges}</span>
         </div>
         <div>
-          <span>Market Cap: </span>
+          <span>Market Cap:</span>
           <span>{quote.USD.total_market_cap}</span>
         </div>
         <div>
-          <span>24h Vol: </span>
+          <span>24h Vol:</span>
           <span>{quote.USD.total_volume_24h}</span>
         </div>
         <div>
-          <span>Dominance: </span>
+          <span>Dominance:</span>
           <span>BTC {btc_dominance} </span>
           <span>ETH {eth_dominance}</span>
         </div>
+      </div>
+      <div className={styles.options}>
+        <div>Currency switch</div>
+        <div>Light/dark</div>
       </div>
     </div>
   );
