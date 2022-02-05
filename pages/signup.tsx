5e794949a5a3e80ce63ee89da7e4fa9f3dc7fa79
Andreasmalleus <andreasmalleus@gmail.com>
2022-02-05 14:17:38 +0200

    typo fix and email input

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface SignupProps {}
 
@@ -12,26 +13,28 @@ const Signup: React.FC<SignupProps> = () => {
           type="text"
           className="border border-slate-200 rounded-md text-xs p-1 w-full mb-2 shadow-sm"
         />
+        <div className="mb-1 text-sm">Email</div>
+        <input
+          type="text"
+          className="border border-slate-200 rounded-md text-xs p-1 w-full mb-2 shadow-sm"
+        />
         <div className="mb-1 text-sm">Password</div>
         <input
           type="password"
-          className="border border-slate-200 rounded-md text-xs p-1 w-full shadow-sm"
+          className="border border-slate-200 rounded-md text-xs p-1 w-full mb-2 shadow-sm"
         />
-        <div className="text-xs mb-2 underline cursor-pointer">
-          Forgot password?
-        </div>
         <div className="mb-1 text-sm">Confirm Password</div>
         <input
           type="password"
           className="border border-slate-200 rounded-md text-xs p-1 w-full mb-2 shadow-sm"
         />
-        <button className="btn-primary mt-3 mb-5 shadow-md">Signup</button>
+        <button className="btn-primary mt-3 mb-5 shadow-md">Sign up</button>
         <div className="flex items-center mb-4">
           <div className="w-1/2 h-1 bg-slate-100 mr-2 rounded"></div>
           <div className="text-slate-200 text-xs">OR</div>
           <div className="w-1/2 h-1 bg-slate-100 ml-2 rounded"></div>
         </div>
-        <div className="flex items-center justify-evenly">
+        <div className="flex items-center justify-evenly mb-4">
           <div className="cursor-pointer">
             <Image src="/icons/facebook.svg" width={30} height={30} />
           </div>
@@ -42,6 +45,11 @@ const Signup: React.FC<SignupProps> = () => {
             <Image src="/icons/google.svg" width={30} height={30} />
           </div>
         </div>
+        <Link href="/login">
+          <div className="text-xs mb-2 underline cursor-pointer text-center">
+            Already have an account?
+          </div>
+        </Link>
       </div>
     </div>
   );
