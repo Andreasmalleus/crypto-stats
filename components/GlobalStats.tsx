f8de94d3b67a5c424ab9e370e27a113f192c99ca
Andreasmalleus <andreasmalleus@gmail.com>
2022-02-09 21:01:10 +0200

    moon icons, temporary currency and dark mode toggle

--- components/GlobalStats.tsx
+++ components/GlobalStats.tsx
@@ -1,5 +1,6 @@
 import { general } from "../data";
 import { formatCurrency } from "../utils/formatCurrency";
+import Image from "next/image";
 
 export const GlobalStats = () => {
   const {
@@ -45,9 +46,15 @@ export const GlobalStats = () => {
           </span>
         </div>
       </div>
-      <div className="flex text-xs">
-        <div className="mr-2">Currency switch</div>
-        <div>Light/dark</div>
+      <div className="flex text-xs items-center">
+        <div className="flex items-center cursor-pointer">
+          <Image src={"/images/EUR.png"} width={15} height={15} />
+          <div className="font-headings ml-1">EUR</div>
+        </div>
+        <div className="font-headings mx-2">/</div>
+        <div className="cursor-pointer">
+          <img src={"/icons/moon-filled.svg"} width={15} height={15} />
+        </div>
       </div>
     </div>
   );
