1b73d6226e300323c3b85c45c4af6964ec1d159b
Andreasmalleus <andreasmalleus@gmail.com>
2022-03-19 13:41:02 +0200

    display user on navbar

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -2,9 +2,16 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { GlobalStats } from "./GlobalStats";
+import { useQuery } from "@apollo/client";
+import { ME_QUERY } from "../graphql/queries";
+import Image from "next/image";
 
 export const Navbar: React.FC = () => {
   const router = useRouter();
+
+  const { data, loading, error } = useQuery(ME_QUERY);
+
+  let isMe = data?.me ? true : false;
 
   return (
     <div>
@@ -27,26 +34,40 @@ export const Navbar: React.FC = () => {
                 Exchanges
               </a>
             </Link>
-            <Link href={"/favorites"}>
-              <a className="text-xs mr-8 transition duration-200 hover:text-slate-400 cursor-pointer">
-                Favorites
-              </a>
-            </Link>
+            {isMe ? (
+              <Link href={"/favorites"}>
+                <a className="text-xs mr-8 transition duration-200 hover:text-slate-400 cursor-pointer">
+                  Favorites
+                </a>
+              </Link>
+            ) : null}
           </div>
         </div>
         <div className="flex items-center font-body">
-          <button
-            className="btn-primary bg-transparent mr-4 text-black"
-            onClick={() => router.push("/login")}
-          >
-            Log in
-          </button>
-          <button
-            className="btn-primary mr-4 shadow-lg"
-            onClick={() => router.push("/signup")}
-          >
-            Sign up
-          </button>
+          {loading ? <div className="text-sm">Loading...</div> : null}
+          {error ? <div className="text-sm">Error...</div> : null}
+
+          {isMe ? (
+            <div className="text-xs mr-2">
+              <Image src="/icons/user.svg" width={30} height={30} />
+            </div>
+          ) : (
+            <>
+              <button
+                className="btn-primary bg-transparent mr-4 text-black"
+                onClick={() => router.push("/login")}
+              >
+                Log in
+              </button>
+              <button
+                className="btn-primary mr-4 shadow-lg"
+                onClick={() => router.push("/signup")}
+              >
+                Sign up
+              </button>
+            </>
+          )}
+
           <input
             className="btn-primary w-48 placeholder-white shadow-lg"
             placeholder="Search"
