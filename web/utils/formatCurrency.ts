72ec17e696489b9f179a93a49528a9b66c27ae16
Andreasmalleus <andreasmalleus@gmail.com>
2022-04-05 15:19:00 +0300

    null check

--- web/utils/formatCurrency.ts
+++ web/utils/formatCurrency.ts
@@ -1,3 +1,6 @@
-export const formatCurrency = (val: number) => {
-    return val.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, ",");
+export const formatCurrency = (val: number | null) => {
+  if(val == null){
+    return 0;
+  }
+  return val.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, ",");
   };
