76ceda7808085468b8eaf785f713beb2c60cee65
Andreasmalleus <andreasmalleus@gmail.com>
2022-04-05 15:14:03 +0300

    more added properties

--- web/types/index.ts
+++ web/types/index.ts
@@ -5,32 +5,41 @@ export type FieldError = {
 
 
 export type Listing = {
-  id : number;
-  circulating_supply: number;
-  cmc_rank: number;
-  date_added: string;
-  last_updated: string;
-  max_supply: number;
+  id: number;
   name: string;
-  num_market_pairs: number;
-  slug: string;
   symbol: string;
-  tags : string[];
+  slug: string;
+  num_market_pairs: number;
+  date_added: Date;
+  tags: string[];
+  max_supply: number;
+  circulating_supply: number;
   total_supply: number;
-  quote : ListingQuote
+  platform?: any;
+  cmc_rank: number;
+  self_reported_circulating_supply?: any;
+  self_reported_market_cap?: any;
+  last_updated: Date;
+  quote : Quote
 }
 
-export type ListingQuote = {
-  USD : {
+export type Quote = {
+  EUR : {
     price: number;
-    volume_24h: number;
-    percent_change_1h: number;
-    percent_change_24h: number;
-    percent_change_7d: number;
-    market_cap: number;
+        volume_24h: number;
+        volume_change_24h: number;
+        percent_change_1h: number;
+        percent_change_24h: number;
+        percent_change_7d: number;
+        percent_change_30d: number;
+        percent_change_60d: number;
+        percent_change_90d: number;
+        market_cap: number;
+        market_cap_dominance: number;
+        fully_diluted_market_cap: number;
+        last_updated: Date;
   }
 }
-
 
 export type Exchange = {
   id: number;
