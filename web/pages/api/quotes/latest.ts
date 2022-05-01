3ec5a495db373316de7c1f65ba7599baf2f041ee
Andreasmalleus <andreasmalleus@gmail.com>
2022-05-01 15:56:30 +0300

    id query parameter

--- web/pages/api/quotes/latest.ts
+++ web/pages/api/quotes/latest.ts
@@ -1,12 +1,21 @@
 import { NextApiResponse, NextApiRequest } from "next";
 
 export default async (req : NextApiRequest, res : NextApiResponse ) => {
-  const {slug} = req.query;
-  if(slug == 'undefined'){
+  const {slug, id} = req.query;
+  let url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?convert=EUR`
+  if(slug && id == 'undefined'){
     return;
   }
+  //if query contains slug append slug as parameter
+  if(slug){
+    url += `&slug=${slug}`
+  }
+  //if query contains id append id as parameter
+  if(id){
+    url += `&id=${id}`
+  }
   const response = await fetch(
-    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=${slug}&convert=EUR`,
+    url,
     {
       method: "GET",
       headers: {
