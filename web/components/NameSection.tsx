ecf2bdfc1bd1362f1dcc62496bdcfaf0510b4112
Andreasmalleus <andreasmalleus@gmail.com>
2022-06-27 17:20:27 +0300

    favorite works

--- web/components/NameSection.tsx
+++ web/components/NameSection.tsx
@@ -1,7 +1,17 @@
+import { useApolloClient, useMutation, useQuery } from "@apollo/client";
 import Image from "next/image";
+import { ClipLoader } from "react-spinners";
+import { FAVORITE_MUTATION, UNFAVORITE_MUTATION } from "../graphql/mutations";
+import { FAVORITES_QUERY } from "../graphql/queries";
+import { checkIfFavorite } from "../utils/checkIfFavorite";
 import { formatDate } from "../utils/formatDate";
+import {
+  handleAddToWatchlist,
+  handleRemoveFromWatchlist,
+} from "../utils/handleWatchlist";
 
 interface NameSectionProps {
+  id: number;
   logo: string;
   name: string;
   symbol: string;
@@ -12,6 +22,7 @@ interface NameSectionProps {
 }
 
 export const NameSection: React.FC<NameSectionProps> = ({
+  id,
   logo,
   name,
   symbol,
@@ -20,6 +31,24 @@ export const NameSection: React.FC<NameSectionProps> = ({
   updated,
   tags,
 }) => {
+  const [removeFromWatchlist] = useMutation(UNFAVORITE_MUTATION);
+  const [addToWatchlist] = useMutation(FAVORITE_MUTATION);
+  const apollo = useApolloClient();
+
+  const { data, loading } = useQuery(FAVORITES_QUERY, {
+    variables: {
+      category: "crypto",
+    },
+  });
+
+  if (!data && loading) {
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <ClipLoader size={40} />
+      </div>
+    );
+  }
+
   return (
     <section className="flex-col w-4/12">
       {/* image and name of the currency */}
@@ -29,9 +58,36 @@ export const NameSection: React.FC<NameSectionProps> = ({
         <div className="text-xs text-slate-600 bg-slate-100 border-none rounded-md p-1 ml-3 mr-3">
           {symbol}
         </div>
-        <div className="border py-1 px-2 rounded-lg">
-          <Image src="/icons/star-empty.svg" alt="" width={15} height={15} />
-        </div>
+        {checkIfFavorite(id, data?.favorites) ? (
+          <div className="border py-1 px-2 rounded-lg">
+            <Image
+              src="/icons/star-full.svg"
+              alt=""
+              width={13}
+              height={13}
+              className="cursor-pointer"
+              onClick={() => handleRemoveFromWatchlist(id, removeFromWatchlist)}
+            />
+          </div>
+        ) : (
+          <div className="border py-1 px-2 rounded-lg">
+            <Image
+              src="/icons/star-empty.svg"
+              alt=""
+              width={13}
+              height={13}
+              className="cursor-pointer"
+              onClick={() =>
+                handleAddToWatchlist(
+                  id,
+                  data?.favorites,
+                  addToWatchlist,
+                  apollo.cache
+                )
+              }
+            />
+          </div>
+        )}
       </div>
       {/* additional information about the currency */}
       <div className="flex mb-6">
