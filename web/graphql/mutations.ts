c6a487e8fce015a075654a99289da8bcee0a911c
Andreasmalleus <andreasmalleus@gmail.com>
2022-07-21 22:24:49 +0300

    cryptoId

--- web/graphql/mutations.ts
+++ web/graphql/mutations.ts
@@ -1,71 +1,71 @@
-import {gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_MUTATION = gql`
-	mutation Login($usernameOrEmail: String!, $password: String!){
-		login(usernameOrEmail: $usernameOrEmail, password: $password){
-			user{
-				id,
-				email,
-				username,
-			}
-			error{
-				field,
-				message,
-			}
-		}
-	}
-`
+  mutation Login($usernameOrEmail: String!, $password: String!) {
+    login(usernameOrEmail: $usernameOrEmail, password: $password) {
+      user {
+        id
+        email
+        username
+      }
+      error {
+        field
+        message
+      }
+    }
+  }
+`;
 
 export const LOGOUT_MUTATION = gql`
-	mutation Logout{
-		logout
-	}
-`
+  mutation Logout {
+    logout
+  }
+`;
 
 export const SIGNUP_MUTATION = gql`
-	mutation Signup($options: SignupInput!){
-		signup(options: $options) {
-			error{
-				field,
-				message
-			}
-			user {
-				id,
-				email,
-				username
-			}
-		}
-	}
-`
+  mutation Signup($options: SignupInput!) {
+    signup(options: $options) {
+      error {
+        field
+        message
+      }
+      user {
+        id
+        email
+        username
+      }
+    }
+  }
+`;
 
 export const UPDATE_USER_MUTATION = gql`
-	mutation UpdateUser($options: UserUpdateInput!){
-		updateUsernameOrEmail(options :$options){
-			user{
-				id,
-				username,
-				email
-			}
-			error{
-				field,
-				message
-			}
-		}
-	}
+  mutation UpdateUser($options: UserUpdateInput!) {
+    updateUsernameOrEmail(options: $options) {
+      user {
+        id
+        username
+        email
+      }
+      error {
+        field
+        message
+      }
+    }
+  }
 `;
 
 export const FAVORITE_MUTATION = gql`
-	mutation Favorite($id: Int!, $category: String!){
-		favorite(id: $id, category:$category){
-			id
-			userId,
-			category
-		}
-	}
-`
+  mutation Favorite($cryptoId: Int!, $category: String!) {
+    favorite(cryptoId: $cryptoId, category: $category) {
+      cryptoId
+      userId
+      category
+    }
+  }
+`;
 
 export const UNFAVORITE_MUTATION = gql`
-	mutation UnFavorite($id: Int!, $category: String!){
-		unfavorite(id: $id, category: $category)
-	}
-`
+  mutation UnFavorite($cryptoId: Int!, $category: String!) {
+    unfavorite(cryptoId: $cryptoId, category: $category)
+  }
+`;
